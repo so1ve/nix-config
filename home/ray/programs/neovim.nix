b59518c8e66ed16c5ec3a7c454aaadf957c9db14
so1ve <58381667+so1ve@users.noreply.github.com>
2026-07-23 17:49:01 +0800

    mosideload nvim config

--- home/ray/programs/neovim.nix
+++ home/ray/programs/neovim.nix
@@ -5,5 +5,9 @@
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
+    sideloadInitLua = true;
   };
+
+  xdg.configFile."nvim".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/nvim";
 }
