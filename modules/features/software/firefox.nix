7f9613691fe3fff37e377aca77d55219843ceac5
so1ve <58381667+so1ve@users.noreply.github.com>
2026-07-25 17:04:48 +0800

    firefox language

--- modules/features/software/firefox.nix
+++ modules/features/software/firefox.nix
@@ -1,7 +1,11 @@
 {
   ray.features."software/firefox" = {
     home = {
-      programs.firefox.enable = true;
+      programs.firefox = {
+        enable = true;
+        languagePacks = [ "zh-CN" ];
+        policies.RequestedLocales = [ "zh-CN" ];
+      };
     };
   };
 }
