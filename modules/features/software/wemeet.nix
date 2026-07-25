048f842fcc86f0fc7b65cabce911a3d8506dcbb1
so1ve <58381667+so1ve@users.noreply.github.com>
2026-07-25 17:31:48 +0800

    use wemeet-xwayland

--- modules/features/software/wemeet.nix
+++ modules/features/software/wemeet.nix
@@ -4,6 +4,18 @@
       { pkgs, ... }:
       {
         home.packages = [ pkgs.wemeet ];
+
+        # Native Wayland entry failed to render shared screen video correctly.
+        # Use xwayland instead
+        xdg.desktopEntries.wemeetapp = {
+          name = "WemeetApp";
+          exec = "${pkgs.wemeet}/bin/wemeet-xwayland %u";
+          icon = "wemeet";
+          terminal = false;
+          categories = [ "AudioVideo" ];
+          mimeType = [ "x-scheme-handler/wemeet" ];
+          settings."Name[zh_CN]" = "腾讯会议";
+        };
       };
   };
 }
