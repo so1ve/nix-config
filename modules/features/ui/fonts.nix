6e1b7fea080b213b323eb5dafa226b891d91ea87
so1ve <58381667+so1ve@users.noreply.github.com>
2026-07-25 22:59:41 +0800

    fix font

--- modules/features/ui/fonts.nix
+++ modules/features/ui/fonts.nix
@@ -1,11 +1,22 @@
 {
   ray.features."ui/fonts" = {
     nixos =
-      { pkgs, ... }:
+      {
+        lib,
+        pkgs,
+        ...
+      }:
       {
         fonts.packages = [
           (pkgs.callPackage ../../../packages/r-maple-mono-nf-cn.nix { })
+          pkgs.noto-fonts-cjk-sans
+          pkgs.noto-fonts-cjk-serif
         ];
+
+        fonts.fontconfig.defaultFonts = {
+          sansSerif = lib.mkAfter [ "Noto Sans CJK SC" ];
+          serif = lib.mkAfter [ "Noto Serif CJK SC" ];
+        };
       };
   };
 }
