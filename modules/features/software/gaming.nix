ad9ede7ce9965e9fe8c2ca3df164779bb6a8a005
so1ve <58381667+so1ve@users.noreply.github.com>
2026-07-25 13:12:30 +0800

    try fix hmcl scaling

--- modules/features/software/gaming.nix
+++ modules/features/software/gaming.nix
@@ -16,8 +16,18 @@
 
     home =
       { pkgs, ... }:
+      let
+        hmclScaled = pkgs.symlinkJoin {
+          name = "hmcl-scaled";
+          paths = [ pkgs.hmcl ];
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram "$out/bin/hmcl" --set GDK_SCALE 2
+          '';
+        };
+      in
       {
-        home.packages = [ pkgs.hmcl ];
+        home.packages = [ hmclScaled ];
 
         programs.mangohud.enable = true;
       };
