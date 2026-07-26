9de0f80d9cc459044632f23f6bd1fb7c36a59e76
so1ve <58381667+so1ve@users.noreply.github.com>
2026-07-26 15:29:27 +0800

    kitty opacity

--- modules/features/software/terminal.nix
+++ modules/features/software/terminal.nix
@@ -16,6 +16,7 @@
           programs.kitty = {
             enable = true;
             settings = {
+              background_opacity = "0.92";
               cursor_trail = 3;
               font_family = "R Maple Mono NF CN";
               hide_window_decorations = "yes";
