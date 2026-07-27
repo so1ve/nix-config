dd7adff5241746dcd54989a01bb04e1b2e823abf
so1ve <58381667+so1ve@users.noreply.github.com>
2026-07-27 13:22:16 +0800

    update

--- flake.nix
+++ flake.nix
@@ -15,16 +15,16 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    ab-download-manager = {
-      url = "github:so1ve/ab-download-manager-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:denful/import-tree";
 
     home-manager = {
       url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    ab-download-manager = {
+      url = "github:so1ve/ab-download-manager-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
