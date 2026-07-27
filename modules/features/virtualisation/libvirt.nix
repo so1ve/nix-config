5777ef7a39a5b832668aa196c305b5138d5aca7c
so1ve <58381667+so1ve@users.noreply.github.com>
2026-07-27 13:27:45 +0800

    freerdp and shared folder

--- modules/features/virtualisation/libvirt.nix
+++ modules/features/virtualisation/libvirt.nix
@@ -6,10 +6,22 @@
         username,
         ...
       }:
+      let
+        windowsShare = "/home/${username}/公共/Windows";
+      in
       {
         programs.virt-manager.enable = true;
 
+        environment.systemPackages = [ pkgs.freerdp ];
+
         users.users.${username}.extraGroups = [ "libvirtd" ];
+
+        systemd.tmpfiles.rules = [
+          "d ${windowsShare} 0770 ${username} users -"
+          "a+ /home/${username} - - - - u:qemu-libvirtd:--x"
+          "a+ /home/${username}/公共 - - - - u:qemu-libvirtd:--x"
+          "a+ ${windowsShare} - - - - u:qemu-libvirtd:rwx,d:u:${username}:rwx,d:u:qemu-libvirtd:rwx,d:m:rwx"
+        ];
 
         virtualisation = {
           libvirtd = {
