da877a6ab1b1c68b8eea94fff8bfa02de3d8250f
so1ve <58381667+so1ve@users.noreply.github.com>
2026-07-29 21:03:45 +0800

    fix uefi-systemd-boot menu scale

--- modules/features/hardware/uefi-systemd-boot.nix
+++ modules/features/hardware/uefi-systemd-boot.nix
@@ -2,7 +2,10 @@
   ray.features."hardware/uefi-systemd-boot" = {
     nixos = {
       boot.loader = {
-        systemd-boot.enable = true;
+        systemd-boot = {
+          enable = true;
+          consoleMode = "1";
+        };
         efi.canTouchEfiVariables = true;
       };
     };
