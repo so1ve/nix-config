1b46fd3f7393bf10492ce9b31863fe9c698d0b87
so1ve <58381667+so1ve@users.noreply.github.com>
2026-07-30 01:08:13 +0800

    merge gaming

--- modules/features/software/gaming.nix
+++ modules/features/software/gaming.nix
@@ -1,21 +1,66 @@
 {
-  ray.features."software/gaming" = {
-    nixos =
-      { pkgs, ... }:
-      {
-        programs = {
-          gamemode.enable = true;
-          gamescope.enable = true;
+  ray.features = {
+    "software/gaming" = {
+      nixos =
+        { pkgs, ... }:
+        {
+          programs = {
+            gamemode.enable = true;
+            gamescope.enable = true;
 
-          steam = {
-            enable = true;
-            extraCompatPackages = [ pkgs.proton-ge-bin ];
+            steam = {
+              enable = true;
+              extraCompatPackages = [ pkgs.proton-ge-bin ];
+            };
           };
         };
+
+      home = {
+        programs.mangohud.enable = true;
+      };
+    };
+
+    "software/netease-cloud-game".home =
+      {
+        config,
+        lib,
+        mkFirefoxPwaInstall,
+        pkgs,
+        ...
+      }:
+      mkFirefoxPwaInstall {
+        inherit config lib pkgs;
+        name = "netease-cloud-game";
+        description = "Install NetEase Cloud Game as a Firefox PWA";
+        manifestUrl = "https://cg.163.com/manifestindex.json";
+        installArgs = [
+          "--document-url"
+          "https://cg.163.com/"
+          "--start-url"
+          "https://cg.163.com/"
+          "--icon-url"
+          "https://cg.163.com/logo2.png"
+          "--name"
+          "网易云游戏"
+          "--description"
+          "网易官方云游戏平台"
+        ];
       };
 
-    home = {
-      programs.mangohud.enable = true;
-    };
+    "software/hmcl".home =
+      { pkgs, ... }:
+      let
+        hmclScaled = pkgs.symlinkJoin {
+          name = "hmcl-scaled";
+          paths = [ pkgs.hmcl ];
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram "$out/bin/hmcl" --set GDK_SCALE 2
+          '';
+        };
+      in
+      {
+        home.packages = [ hmclScaled ];
+      };
   };
 }
