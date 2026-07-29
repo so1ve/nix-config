9ddfdcc95b68e6ced24596bd930971ccb55aa660
so1ve <58381667+so1ve@users.noreply.github.com>
2026-07-29 18:33:56 +0800

    presentation operations

--- modules/features/software/kde-connect.nix
+++ modules/features/software/kde-connect.nix
@@ -1,3 +1,6 @@
+let
+  phone = "ad448fa3108d4a268f16bdf9cd1132d6";
+in
 {
   ray.features."software/kde-connect" = {
     nixos = {
@@ -7,16 +10,45 @@
       };
     };
 
-    home = {
-      services.kdeconnect = {
-        enable = true;
-        indicator = true;
-      };
+    home =
+      { pkgs, ... }:
+      let
+        commands = builtins.toJSON {
+          presenter-exit = {
+            command = "${pkgs.xdotool}/bin/xdotool key Escape";
+            name = "Presentation: Exit";
+          };
+          presenter-next = {
+            command = "${pkgs.xdotool}/bin/xdotool key Next";
+            name = "Presentation: Next";
+          };
+          presenter-previous = {
+            command = "${pkgs.xdotool}/bin/xdotool key Prior";
+            name = "Presentation: Previous";
+          };
+          presenter-start = {
+            command = "${pkgs.xdotool}/bin/xdotool key F5";
+            name = "Presentation: Start";
+          };
+        };
+      in
+      {
+        home.packages = [ pkgs.xdotool ];
 
-      xdg.mimeApps = {
-        enable = true;
-        defaultApplications."x-scheme-handler/kdeconnect" = "org.kde.dolphin.desktop";
+        services.kdeconnect = {
+          enable = true;
+          indicator = true;
+        };
+
+        xdg.configFile."kdeconnect/${phone}/kdeconnect_runcommand/config".text = ''
+          [General]
+          commands="@ByteArray(${builtins.replaceStrings [ "\"" ] [ "\\\"" ] commands})"
+        '';
+
+        xdg.mimeApps = {
+          enable = true;
+          defaultApplications."x-scheme-handler/kdeconnect" = "org.kde.dolphin.desktop";
+        };
       };
-    };
   };
 }
