d4ceaff76e4b1d74d594cb24906b1bbef0d50176
so1ve <58381667+so1ve@users.noreply.github.com>
2026-07-31 01:45:09 +0800

    update snapshot limit

--- modules/features/storage/snapper.nix
+++ modules/features/storage/snapper.nix
@@ -3,24 +3,32 @@
     nixos =
       { username, ... }:
       let
-        mkSnapperConfig = subvolume: {
-          SUBVOLUME = subvolume;
+        mkSnapperConfig =
+          {
+            subvolume,
+            numberLimit ? 10,
+            daily,
+            weekly,
+            monthly,
+          }:
+          {
+            SUBVOLUME = subvolume;
 
-          ALLOW_USERS = [ username ];
-          SYNC_ACL = true;
+            ALLOW_USERS = [ username ];
+            SYNC_ACL = true;
 
-          NUMBER_CLEANUP = true;
-          NUMBER_LIMIT = 10;
-          NUMBER_LIMIT_IMPORTANT = 3;
+            NUMBER_CLEANUP = true;
+            NUMBER_LIMIT = numberLimit;
+            NUMBER_LIMIT_IMPORTANT = 3;
 
-          TIMELINE_CREATE = true;
-          TIMELINE_CLEANUP = true;
-          TIMELINE_LIMIT_HOURLY = 6;
-          TIMELINE_LIMIT_DAILY = 7;
-          TIMELINE_LIMIT_WEEKLY = 4;
-          TIMELINE_LIMIT_MONTHLY = 3;
-          TIMELINE_LIMIT_YEARLY = 0;
-        };
+            TIMELINE_CREATE = true;
+            TIMELINE_CLEANUP = true;
+            TIMELINE_LIMIT_HOURLY = 3;
+            TIMELINE_LIMIT_DAILY = daily;
+            TIMELINE_LIMIT_WEEKLY = weekly;
+            TIMELINE_LIMIT_MONTHLY = monthly;
+            TIMELINE_LIMIT_YEARLY = 0;
+          };
       in
       {
         services.snapper = {
@@ -28,8 +36,20 @@
           persistentTimer = true;
 
           configs = {
-            root = mkSnapperConfig "/";
-            home = mkSnapperConfig "/home";
+            root = mkSnapperConfig {
+              subvolume = "/";
+              numberLimit = 5;
+              daily = 3;
+              weekly = 2;
+              monthly = 1;
+            };
+
+            home = mkSnapperConfig {
+              subvolume = "/home";
+              daily = 7;
+              weekly = 4;
+              monthly = 2;
+            };
           };
         };
       };
