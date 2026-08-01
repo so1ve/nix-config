8d55a82e8556e2982be1c4cb2fe2119d4dc2ae6a
so1ve <58381667+so1ve@users.noreply.github.com>
2026-08-02 00:54:46 +0800

    move podman containers to /var/lib/containers/

--- modules/features/virtualisation/podman.nix
+++ modules/features/virtualisation/podman.nix
@@ -1,7 +1,11 @@
 {
   ray.features."virtualisation/podman" = {
     nixos =
-      { pkgs, ... }:
+      {
+        pkgs,
+        username,
+        ...
+      }:
       {
         virtualisation.podman = {
           enable = true;
@@ -11,6 +15,21 @@
 
           extraPackages = [ pkgs.podman-compose ];
         };
+
+        systemd.tmpfiles.rules = [
+          "d /var/lib/containers/${username} 0700 ${username} users - -"
+          "d /var/lib/containers/${username}/storage 0700 ${username} users - -"
+        ];
+      };
+
+    home =
+      { username, ... }:
+      {
+        xdg.configFile."containers/storage.conf".text = ''
+          [storage]
+          driver = "overlay"
+          graphroot = "/var/lib/containers/${username}/storage"
+        '';
       };
   };
 }
