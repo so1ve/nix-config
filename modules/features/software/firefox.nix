cbefa091adcd23d67c77fbadc1b5dd2830d16cd4
so1ve <58381667+so1ve@users.noreply.github.com>
2026-08-05 02:13:15 +0800

    allow some domains

--- modules/features/software/firefox.nix
+++ modules/features/software/firefox.nix
@@ -8,6 +8,7 @@
       };
 
       xdg.dataFile."firefoxpwa/profiles/00000000000000000000000000/user.js".text = ''
+        user_pref("firefoxpwa.allowedDomains", "accounts.google.com,addons.mozilla.org,auth.zulipchat.com,github.com");
         user_pref("firefoxpwa.dynamicWindowTitle", false);
         user_pref("firefoxpwa.launchType", 3);
         user_pref("firefoxpwa.openOutOfScopeInDefaultBrowser", true);
