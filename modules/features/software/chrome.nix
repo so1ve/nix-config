1795eca003b580081af6e1452076fdc1001438ce
so1ve <58381667+so1ve@users.noreply.github.com>
2026-08-05 19:14:19 +0800

    fix pwa focus or launch

--- modules/features/software/chrome.nix
+++ modules/features/software/chrome.nix
@@ -4,28 +4,30 @@
       { config, lib, ... }:
       {
         options.ray.chromeWebApps = lib.mkOption {
-          type = lib.types.listOf (lib.types.submodule {
-            freeformType = lib.types.attrsOf lib.types.anything;
-            options = {
-              url = lib.mkOption {
-                type = lib.types.str;
+          type = lib.types.listOf (
+            lib.types.submodule {
+              freeformType = lib.types.attrsOf lib.types.anything;
+              options = {
+                url = lib.mkOption {
+                  type = lib.types.str;
+                };
+                custom_name = lib.mkOption {
+                  type = lib.types.str;
+                };
+                create_desktop_shortcut = lib.mkOption {
+                  type = lib.types.bool;
+                  default = true;
+                };
+                default_launch_container = lib.mkOption {
+                  type = lib.types.enum [
+                    "tab"
+                    "window"
+                  ];
+                  default = "window";
+                };
               };
-              custom_name = lib.mkOption {
-                type = lib.types.str;
-              };
-              create_desktop_shortcut = lib.mkOption {
-                type = lib.types.bool;
-                default = true;
-              };
-              default_launch_container = lib.mkOption {
-                type = lib.types.enum [
-                  "tab"
-                  "window"
-                ];
-                default = "window";
-              };
-            };
-          });
+            }
+          );
           default = [ ];
           description = "Web apps force-installed by Google Chrome policy.";
         };
@@ -36,11 +38,46 @@
       };
 
     home =
-      { lib, pkgs, ... }:
+      {
+        lib,
+        mkFocusOrLaunch,
+        pkgs,
+        ...
+      }:
+      let
+        chrome = pkgs.google-chrome;
+        focusOrLaunch = mkFocusOrLaunch pkgs;
+        chromeLauncher = pkgs.writeShellApplication {
+          name = "google-chrome-stable";
+          text = ''
+            app_id=""
+            profile="Default"
+
+            for argument in "$@"; do
+              case "$argument" in
+                --app-id=*) app_id="''${argument#--app-id=}" ;;
+                --profile-directory=*) profile="''${argument#--profile-directory=}" ;;
+              esac
+            done
+
+            if [[ -n "$app_id" ]]; then
+              profile="''${profile// /_}"
+              exec ${lib.getExe focusOrLaunch} \
+                "chrome-$app_id-$profile" \
+                ${lib.getExe chrome} "$@"
+            fi
+
+            exec ${lib.getExe chrome} "$@"
+          '';
+        };
+      in
       {
         home = {
-          packages = [ pkgs.google-chrome ];
-          sessionVariables.BROWSER = lib.getExe pkgs.google-chrome;
+          packages = [
+            (lib.hiPrio chromeLauncher)
+            chrome
+          ];
+          sessionVariables.BROWSER = lib.getExe chrome;
         };
 
         xdg.mimeApps = {
