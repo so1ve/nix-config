750923478b2c04327612b9a43ac44b2675b78429
so1ve <58381667+so1ve@users.noreply.github.com>
2026-08-10 16:23:37 +0800

    stup tailscale operator and tray

--- modules/features/software/tailscale.nix
+++ modules/features/software/tailscale.nix
@@ -1,8 +1,15 @@
 {
   ray.features."software/tailscale".nixos =
-    { config, ... }:
     {
-      services.tailscale.enable = true;
+      config,
+      username,
+      ...
+    }:
+    {
+      services.tailscale = {
+        enable = true;
+        extraSetFlags = [ "--operator=${username}" ];
+      };
 
       networking.firewall = {
         trustedInterfaces = [ config.services.tailscale.interfaceName ];
@@ -12,5 +19,25 @@
       systemd.services.tailscaled.serviceConfig.Environment = [
         "TS_DEBUG_FIREWALL_MODE=nftables"
       ];
+    };
+
+  ray.features."software/tailscale".home =
+    { pkgs, ... }:
+    {
+      systemd.user.services.tailscale-systray = {
+        Unit = {
+          Description = "Tailscale system tray";
+          After = [ "graphical-session.target" ];
+          PartOf = [ "graphical-session.target" ];
+        };
+
+        Service = {
+          ExecStart = "${pkgs.tailscale}/bin/tailscale systray";
+          Restart = "on-failure";
+          RestartSec = 3;
+        };
+
+        Install.WantedBy = [ "graphical-session.target" ];
+      };
     };
 }
