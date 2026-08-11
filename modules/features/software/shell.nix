53c556aa1b5094e2f642789a0ae25118b8e69eb4
so1ve <58381667+so1ve@users.noreply.github.com>
2026-08-11 22:49:40 +0800

    compat code

--- modules/features/software/shell.nix
+++ modules/features/software/shell.nix
@@ -36,28 +36,18 @@
               set -g fish_greeting
 
               function __fish_set_tmux_pane_state --argument-names state
-                if set -q TMUX TMUX_PANE
-                  command tmux set-option -pt "$TMUX_PANE" @pane-is-fish $state 2>/dev/null
-                end
+                set -q TMUX_PANE; and command tmux set-option -pt "$TMUX_PANE" @pane-is-fish $state 2>/dev/null
               end
 
-              function __fish_tmux_preexec --on-event fish_preexec
-                __fish_set_tmux_pane_state 0
-              end
-
-              function __fish_tmux_postexec --on-event fish_postexec
-                __fish_set_tmux_pane_state 1
-              end
-
+              function __fish_tmux_preexec --on-event fish_preexec; __fish_set_tmux_pane_state 0; end
+              function __fish_tmux_postexec --on-event fish_postexec; __fish_set_tmux_pane_state 1; end
               __fish_set_tmux_pane_state 1
 
               function __fish_complete_or_navigate_tmux --argument-names direction input_function
-                if commandline --paging-mode
+                if commandline --paging-mode; or not set -q TMUX
                   commandline -f $input_function
-                else if set -q TMUX
-                  command tmux select-pane -$direction
                 else
-                  commandline -f $input_function
+                  command tmux select-pane -$direction
                 end
               end
 
