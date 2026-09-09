bf5818b85130bb1fe2c02b1d551e0b470964f49d
so1ve <58381667+so1ve@users.noreply.github.com>
2026-09-09 12:24:10 +0800

    wireguard

--- modules/features/software/wireguard.nix
+++ modules/features/software/wireguard.nix
@@ -3,11 +3,36 @@
     requires = [ "security/agenix" ];
 
     nixos =
-      { inputs, pkgs, ... }:
+      { config, inputs, ... }:
       {
         age.secrets.wireguard-private-key.file = "${inputs.self}/secrets/wireguard-private-key.age";
 
-        environment.systemPackages = [ pkgs.wireguard-tools ];
+        networking.wg-quick.interfaces.school = {
+          autostart = false;
+          address = [ "192.168.66.3/32" ];
+          privateKeyFile = config.age.secrets.wireguard-private-key.path;
+
+          peers = [
+            {
+              publicKey = "nbpoSOiKhM2Yy58dGsRp5ORxTzKJtD0lSel5CjP9hGs=";
+              allowedIPs = [
+                "192.168.66.1/32"
+                "192.168.2.143/32"
+              ];
+              endpoint = "10.1.139.2:51821";
+              persistentKeepalive = 25;
+            }
+          ];
+        };
       };
+
+    home = {
+      programs.fish.shellAbbrs = {
+        wgup = "sudo systemctl start wg-quick-school.service";
+        wgdown = "sudo systemctl stop wg-quick-school.service";
+        wgstatus = "sudo wg show school";
+        wglog = "sudo journalctl -u wg-quick-school.service -b -e";
+      };
+    };
   };
 }
