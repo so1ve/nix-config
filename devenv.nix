c4de348619f3897160270cf4b03ac37a0bf98e43
so1ve <58381667+so1ve@users.noreply.github.com>
2026-09-11 03:37:01 +0800

    add profile

--- devenv.nix
+++ devenv.nix
@@ -4,6 +4,8 @@
     /home/ray/.config/devenv/ray/profiles/config.nix
     /home/ray/.config/devenv/ray/profiles/nix.nix
     /home/ray/.config/devenv/ray/profiles/lua.nix
+    /home/ray/.config/devenv/ray/profiles/frontend.nix
+    /home/ray/.config/devenv/ray/profiles/python.nix
   ]
   ++ lib.optional (builtins.pathExists ./devenv.local.nix) ./devenv.local.nix;
 }
