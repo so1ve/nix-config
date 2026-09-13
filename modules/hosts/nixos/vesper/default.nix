8c9296b1085983c84732e23af5f908531f500d43
so1ve <58381667+so1ve@users.noreply.github.com>
2026-09-13 19:00:50 +0800

    Revert "disable wireguard"
    This reverts commit 3dfea0bec39af2b19a537777ca140c2819af57c1.

--- modules/hosts/nixos/vesper/default.nix
+++ modules/hosts/nixos/vesper/default.nix
@@ -73,7 +73,7 @@
       "software/chrome"
       "software/mihomo"
       "software/tailscale"
-      # "software/wireguard"
+      "software/wireguard"
       "software/ab-download-manager"
       "software/qbittorrent"
       "software/kde-connect"
