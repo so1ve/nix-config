da43fe0a58e720fd62156a2b51a3d52bf6ec50bf
so1ve <58381667+so1ve@users.noreply.github.com>
2026-09-21 17:04:34 +0800

    downgrade bluez to workaround a bug

--- modules/features/desktop/media.nix
+++ modules/features/desktop/media.nix
@@ -1,18 +1,24 @@
 {
   ray.features."desktop/media" = {
-    nixos = {
-      services = {
-        pulseaudio.enable = false;
+    nixos =
+      { inputs, pkgs, ... }:
+      {
+        # FIXME: Remove this pin once the Bluetooth reconnect/audio regression is fixed upstream.
+        hardware.bluetooth.package =
+          inputs.so1ve.packages.${pkgs.stdenv.hostPlatform.system}."bluez-5_86";
 
-        pipewire = {
-          enable = true;
-          alsa.enable = true;
-          alsa.support32Bit = true;
-          pulse.enable = true;
+        services = {
+          pulseaudio.enable = false;
+
+          pipewire = {
+            enable = true;
+            alsa.enable = true;
+            alsa.support32Bit = true;
+            pulse.enable = true;
+          };
         };
-      };
 
-      security.rtkit.enable = true;
-    };
+        security.rtkit.enable = true;
+      };
   };
 }
